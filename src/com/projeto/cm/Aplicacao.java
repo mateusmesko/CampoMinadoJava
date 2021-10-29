381ee556927c658cc248251eb3fb24a09c90dc51
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-29 19:08:03 -0300

    Update Aplicacao.java

--- src/com/projeto/cm/Aplicacao.java
+++ src/com/projeto/cm/Aplicacao.java
@@ -6,7 +6,7 @@ import com.projeto.cm.visao.TabuleiroConsole;
 public class Aplicacao {
 	
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 4);
+		Tabuleiro tabuleiro = new Tabuleiro(10, 6, 4);
 		
 		new TabuleiroConsole(tabuleiro);
 	
