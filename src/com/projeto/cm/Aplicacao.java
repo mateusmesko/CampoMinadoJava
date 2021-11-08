81046437bd6b7828a50d3a0f5e741d218a675f1c
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-11-07 23:46:57 -0300

    Update Aplicacao.java

--- src/com/projeto/cm/Aplicacao.java
+++ src/com/projeto/cm/Aplicacao.java
@@ -2,7 +2,7 @@ package com.projeto.cm;
  
 import com.projeto.cm.modelo.Tabuleiro;
 import com.projeto.cm.visao.TabuleiroConsole;
-
+ 
 public class Aplicacao {
 	
 	public static void main(String[] args) {
